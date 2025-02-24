7a46a4a1f5a2e2b2e99401b9e44a1c6f8ecdcfc2
Marat Ayupov <maratayup@yandex.ru>
2025-02-24 14:42:20 +0300

    создал класс Document и сделал форму

--- AyupovCourseProject/TestClass.cs
+++ AyupovCourseProject/TestClass.cs
@@ -6,16 +6,35 @@ using System.Threading.Tasks;
 
 namespace AyupovCourseProject
 {
-    public class TestClass
+    public class Document
     {
+        public int Id { get; private set; }
+        public string Title { get; set; }
+        public string SenderName { get; set; }
+        public string ReceiptDate { get; set; }
+        public string DocumentTopic { get; set; }
+        public string DocumentContent { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
-        public TestClass() { }
-        public TestClass(string name, string description) { Name = name; Description = description; }
+
+        private static int countOfObjects = 0;
+        public Document() 
+        {
+            this.Id = countOfObjects;
+            countOfObjects++;
+        }
+        public Document(string title, string senderName, string receiptDate, string documentTopic, string documentContent) 
+        {
+            this.Title = title;
+            this.SenderName = senderName;
+            this.ReceiptDate = receiptDate;
+            this.DocumentTopic = documentTopic;
+            this.DocumentContent = documentContent;
+        }
 
         public override string ToString()
         {
-            return Name + " | " + Description;
+            return Name;
         }
+        
     }
 }
